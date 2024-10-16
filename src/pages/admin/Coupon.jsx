6a3476100f150c7d380645ef100037179ed47088
amanuelcm27 <amanuelfirew27@gmail.com>
2024-10-16 04:23:50 +0300

    coupon creation logic complete

--- src/pages/admin/Coupon.jsx
+++ src/pages/admin/Coupon.jsx
@@ -1,14 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminProductCard from "../../components/AdminProductCard";
 import images from "../../constants/images";
 import FormField from "../../components/FormField";
 import SubmitButton from "../../components/SubmitButton";
-
+import useFormHandler from "../../handlers/useFormHandler";
+import { apiRequest } from "../../handlers/apiHandler";
+import InfoCard from "../../components/InfoCard";
 const Coupon = () => {
-  const [expandOrder, setExpandOrder] = useState(false);
+  const [couponForm, setExpandCouponForm] = useState(false);
   const [deleteBox, setDeleteBox] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [info, setInfo] = useState("");
+  const [error, setError] = useState(false);
+  const [infokey, setInfoKey] = useState(0);
+  const [formData, handleChange , clearForm] = useFormHandler({
+    code: "",
+    discount: "",
+    product: null,
+    days: "",
+  });
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const fetchProducts = async () => {
+    const response = await apiRequest("get", "list_products/");
+    if (response.success === false) {
+      setError(true);
+    } else {
+      setProducts(response);
+    }
+  };
+  const formValid = () => {
+    if (
+      formData.code === "" ||
+      formData.discount === "" 
+    ) {
+      setInfo(" coupon code and discount are required fields"); 
+      setError(true);
+      return false;
+    }
+    return formData;
+  };
+  const createCoupon = async () => {
+    const validatedData = formValid();
+    if (validatedData) {
+      const response = await apiRequest("post", "create_coupon/", validatedData);
+      if (response.success === false) {
+        console.log(response);
+        setError(true);
+        setInfo("Error in creating coupon ensure you have not used this code before");
+        console.log(response);
+      } else {
+        setInfo("Coupon has been created");
+
+        console.log(response);
+      }
+    }
+
+    setInfoKey(infokey + 1);
+  };
+  const setProduct = (product) => {
+    handleChange({
+      target: {
+        type: "text",
+        name: "product",
+        value: product.id,
+      },
+    });
+    setSelectedProduct(product.name);
+  };
+  const fetchCoupons = async () => {  
+    const response = await apiRequest("get", "coupons/");
+    if (response.success === false) {
+      setError(true);
+      setInfo("Error in fetching coupons");
+    } else {
+      setInfo("Coupons fetched successfully");
+      console.log(response);
+    }
+    setInfoKey(infokey + 1);
+  }
+  useEffect(() => {
+    fetchProducts();
+    fetchCoupons();
+  }, []);
   return (
     <>
+      <InfoCard info={info} iserror={error} infokey={infokey} />
       <div className="w-[85%] h-[600px] ">
         <div className="h-full overflow-y-scroll">
           <div className=" border-b-2 flex m-8 items-center">
@@ -16,7 +92,7 @@ const Coupon = () => {
               Products with Coupons
             </span>
             <span
-              onClick={() => setExpandOrder(!expandOrder)}
+              onClick={() => setExpandCouponForm(!couponForm)}
               className="hover:bg-gray-200  rounded-lg m-2 cursor-pointer p-2"
             >
               <i className="fa-solid fa-plus"></i> New Coupon
@@ -25,36 +101,60 @@ const Coupon = () => {
           <AdminProductCard
             title="delete"
             icon={`fa-solid fa-trash hover:text-red-700`}
-            status={`Active`}
+            info={`Active`}
             styles={`text-green-500 font-extrabold`}
             handleClick={() => setDeleteBox(!deleteBox)}
           />
         </div>
       </div>
-      {expandOrder && (
+      {couponForm && (
         <div className="absolute flex justify-center items-center right-0 w-full h-[600px] bg-white bg-opacity-75">
           <div className="w-[40%] h-[500px] overflow-y-scroll  bg-white shadow-custom border-2 rounded-md">
             <div className="m-4 flex font-bold items-center">
               <span className="flex-1">Add a coupon</span>
               <i
-                onClick={() => setExpandOrder(!expandOrder)}
+                onClick={() => setExpandCouponForm(!couponForm)}
                 class="fa-regular hover:scale-110 cursor-pointer fa-circle-xmark"
               ></i>
             </div>
             <div className="flex flex-col m-4 ">
-              <FormField placeholder="coupon code" type="text" />
+              <FormField
+                placeholder="coupon code"
+                type="text"
+                name="code"
+                value={formData.code}
+                handleChange={handleChange}
+              />
               <div className="m-2">
                 <span>Product this coupon to be applied on:</span>
+                <div className="text-gray-500 text-sm my-2">by default its for  all products in particular cart</div>
                 <div className="relative group flex flex-col bg-gray-100  cursor-pointer">
                   <div className="flex p-2 hover:bg-gray-200">
-                    <span className="flex-1">Choose a product</span>
+                    <span className="flex-1">
+                      {selectedProduct || "Choose a product "}
+                    </span>
                     <span>
                       <i className="fa-solid fa-caret-down"></i>
                     </span>
                   </div>
                   <div className="absolute top-10  flex-col pointer-events-none hidden group-hover:flex group-hover:pointer-events-auto  right-0 w-full bg-gray-100 border-t-2 h-[100px] overflow-y-scroll">
-                    <span className=" p-2 hover:bg-gray-300">All products</span>
-                    <span className=" p-2 hover:bg-gray-300">Product 1</span>
+                    <span
+                      onClick={() =>
+                        setSelectedProduct("All products in a cart")
+                      }
+                      className=" p-2 hover:bg-gray-300"
+                    >
+                      All products in a cart
+                    </span>
+                    {products?.map((product) => (
+                      <span
+                        key={product.id}
+                        onClick={() => setProduct(product)}
+                        className="p-2 hover:bg-gray-300"
+                      >
+                       For {product.name}
+                      </span>
+                    ))}
                   </div>
                 </div>
               </div>
@@ -64,21 +164,34 @@ const Coupon = () => {
                   <input
                     className="bg-gray-200 mx-2 text-right appearance-none outline-none "
                     type="number"
-                    name="expiry"
+                    name="days"
+                    value={formData.days}
+                    onChange={handleChange}
                   />
                   days
+                  <span className="text-gray-500">
+                    (the default is 15 days)
+                  </span>
                 </span>
               </div>
               <div className="flex flex-col m-2 justify-center">
                 <span>Discount amount (in %) : </span>
-                <FormField type="number" placeholder="in percentage (%)" />
+                <FormField
+                  type="number"
+                  name="discount"
+                  placeholder="in percentage (%)"
+                  value={formData.discount}
+                  handleChange={handleChange}
+                />
               </div>
               <div className="flex mt-12">
                 <SubmitButton
+                  handleSubmit={() =>{ createCoupon() , clearForm() }}
                   otherStyles={`bg-green-400 mx-2 w-full`}
                   name={`save & add another`}
                 />
                 <SubmitButton
+                  handleSubmit={() => {createCoupon() , setExpandCouponForm(!couponForm)}}
                   otherStyles={`bg-orange-400 w-full`}
                   name={`Save Coupon`}
                 />
@@ -103,9 +216,16 @@ const Coupon = () => {
                 <span className="text-green-600">coupon name</span>
               </div>
               <div className="flex mt-auto">
-                <SubmitButton handleSubmit={()=>setDeleteBox(!deleteBox)} name={`Cancel`} otherStyles="bg-green-400 rounded-full" />
-                <SubmitButton name={`I'm Sure`} otherStyles=" mx-2 rounded-full bg-red-400" />
-              </div> 
+                <SubmitButton
+                  handleSubmit={() => setDeleteBox(!deleteBox)}
+                  name={`Cancel`}
+                  otherStyles="bg-green-400 rounded-full"
+                />
+                <SubmitButton
+                  name={`I'm Sure`}
+                  otherStyles=" mx-2 rounded-full bg-red-400"
+                />
+              </div>
             </div>
           </div>
         </div>
