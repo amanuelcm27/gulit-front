0ba7b051e285f2205454634746bb7df018b4e3f0
amanuelcm27 <amanuelfirew27@gmail.com>
2024-11-13 16:20:20 +0300

    test oauth id

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ import PaymentVerify from "./pages/store/PaymentVerify";
 function App() {
   const [count, setCount] = useState(0);
   const OauthId = import.meta.env.VITE_OAUTH_CLIENT_ID;
+  console.log(OauthId)
   return (
     <>
       <Router>
